7dc4de07f464a42caf38cf724d45390150f2f398
YawRaz <riclerico@gmail.com>
2024-08-09 13:17:11 +0000

    ref: add report file and commandline option

--- FileInventory.ps1
+++ FileInventory.ps1
@@ -22,9 +22,13 @@ Specifies the ditecrory to start the traversal from
 .PARAMETER outFile
 Specifies the file to write the results to.
 
+.PARAMETER reportTitle
+Specifies the HTML report title
 
 .EXAMPLE 
 FileInventory ComputerName
+
+.EXAMPLE FileInventory -reportTitle "Computer Local Files Report"
 #>
 
 # Parameter Definition Section
@@ -32,13 +36,38 @@ param (
 	[string]$UserName
 	[string]$targetComputer
 	[string]$outFile
+	[string]$reportTitle
 
 # local variable definition
 # Get the current data and time
 $rptDate=Get-Date
 $epoch=([DateTimeOffset]$rptDate).ToUnixTimeSeconds()
 
+# create HTML section with header
+$Header = @"
+
+<style>
+TABLE {
+	border-width: 1px;
+	border-style: solid;
+	border-color: black;
+	border-collapse: Collapse;
+      }
+TD {
+	border-width: 1px;
+	padding: 3px;
+	border-style: solid;
+	border-color: black;
+   }
+</style>
+<p><b>$reportTitle $rptDate <P>
+File Section: <b>$targetDirectory</b><p>
+Target Computer: <b>$targetComputer </b>
+<p>
+"@
+
 # report file creation
+$ReportFile = ".\Report-"+$epoch+".HTML"
 
 # cmdLet Command Line execution
-Invoke-Command -ComputerName $targetComputer -Credential $UserName -ScriptBlock { Get-ChildItem -Path $startDirectory -Recurse -Force }
+Invoke-Command -ComputerName $targetComputer -Credential $UserName -ScriptBlock { Get-ChildItem -Path $startDirectory -Recurse -Force } | ConvertTo-HTML -Head $Header -Property TimeGenerated Message | outFile $ReportFile                 
